a5fd72c0a24a7d1ee9d7a458587579b2001afa18
Avx <szoviktor40@gmail.com>
2023-11-30 09:30:09 +0100

    Kerekpar

---
+++ HegyiKerekpar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HegyiKerekparName
+{
+    class HegyiKerekpar
+    {
+        public string Rajtszam { get; set; }
+        public string Kategoria { get; set; }
+        public string Nev { get; set; }
+        public string Egyesulet { get; set; }
+        public TimeSpan Ido { get; set; }
+
+
+        public HegyiKerekpar(string sor)
+        {
+            string[] data = sor.Split(';');
+            this.Rajtszam = data[0];
+            this.Kategoria = data[1];
+            this.Nev = data[2];
+            this.Egyesulet = data[3];
+            var ido = (data[4].Split(":"));
+            this.Ido = new TimeSpan(int.Parse(ido[0]), int.Parse(ido[1]), int.Parse(ido[2]));
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,19 +1,47 @@
-﻿class HegyiKerekpar
+﻿using HegyiKerekparName;
+using VersenyTavNev;
+using System.Text;
+
+List<HegyiKerekpar> lista = new List<HegyiKerekpar>();
+
+StreamReader sr = new StreamReader("../../../src/bukkm2019.txt", encoding: Encoding.UTF8);
+
+var _ = sr.ReadLine();
+
+while (!sr.EndOfStream)
 {
-    public string Rajtszam{ get; set; }
-    public string Kategoria { get; set; }
-    public string Nev { get; set; }
-    public string Egyesulet { get; set; }
-    public TimeSpan Ido { get; set; }
+    lista.Add(new HegyiKerekpar(sr.ReadLine()));
+}
 
-    public HegyiKerekpar(string sor)
+var f4 = (double)(691-lista.Count) / (double)691*100;
+Console.WriteLine($"A versenyzők {f4} arányban teljesitették");
+
+var f5 = lista.Where(x => x.Rajtszam.StartsWith("R") && x.Kategoria.EndsWith("n")).Count();
+Console.WriteLine($"Rövidtávú versenyen elindult nők száma: {f5} fő");
+
+var f6 = lista.Any(x => x.Ido.Hours >= 6);
+
+Console.WriteLine($"{(f6 ? "Volt ilyen" : "Nem volt ilyen")}");
+
+var f7 = lista.Where(x => x.Rajtszam.StartsWith("R") && x.Kategoria.EndsWith("f")).OrderBy(x => x.Ido.Hours).ToList().First();
+
+Console.WriteLine($"{f7.Rajtszam} {f7.Nev}");
+
+Dictionary<string, int> f8 = new Dictionary<string, int>();
+foreach (var item in lista)
+{
+    if (f8.ContainsKey(item.Kategoria))
     {
-        string[] data = sor.Split(';');
-        this.Rajtszam = data[0];
-        this.Kategoria = data[1];
-        this.Nev = data[2];
-        this.Egyesulet = data[3];
-        this.Ido = d;
+        f8[item.Kategoria] += 1;
     }
+    else
+    {
+        f8.Add(item.Kategoria, 1);
+    }
+    
+}
 
+foreach (var item in f8)
+{
+    Console.WriteLine(item);
 }
